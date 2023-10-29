a486fd5a9611b101d31b1eaddadf3c87a037f622
brad@smithwebtek.com <brad@smithwebtek.com>
2023-10-28 23:05:19 -0400

    Vitest draft v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([])
+
+  useEffect(()=>{
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((json) => setTodos(json));
+  },[])
+
+  console.log('[App] todos: ', todos);
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          <h3>{todo.id}: {todo.title}</h3>
+          <h3>{todo.id}</h3>
+          <p>{todo.completed ? 'DONE' : 'TODO'}</p>
+        </div>
+      ))}
     </>
   )
 }
